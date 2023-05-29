43d71b1e6394d662077487c267d6ea026db6a686
Zurceel <gabrielcruzsil@gmail.com>
2023-05-29 11:07:44 -0300

    Adicionar arquivos de projeto.

---
+++ MeuPrimeiroPrograma/Program.cs
@@ -0,0 +1,11 @@
+﻿const string textoOlamundo = "Olá Mundo, esse é meu primeiro programa C#!, feito por ";
+string nomeComple;
+int idade;
+
+Console.WriteLine("Informe seu nome completo");
+nomeComple = Console.ReadLine().ToString();
+
+Console.WriteLine("Informe sua idade");
+idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(textoOlamundo + nomeComple + "que tem " + idade + " anos");
